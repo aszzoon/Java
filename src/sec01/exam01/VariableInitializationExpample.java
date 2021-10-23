8d255e1b1d81c193015c1938e6ff2d4eca84f8c4
boxer <aszzoon@naver.com>
2021-10-24 00:09:15 +0900

    VariableInitializationExample

---
+++ src/sec01/exam01/VariableInitializationExpample.java
@@ -0,0 +1,15 @@
+package sec01.exam01;
+
+public class VariableInitializationExpample {
+    public static void main(String[] args) {
+        //변수 value 선언
+        int value = 0;
+
+        //변수 value 값을 읽고 10일 더하는 산술 연산을 수행
+        //연산의 결과값을 변수 result의 초기값을 저징
+        int result = value + 10;
+
+        //변수 result 값을 읽고 콘솔에 출력
+        System.out.println(result);
+    }
+}
